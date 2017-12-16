76eee5a2eba96ac5b234d00152850be8778e5027
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-12-16 10:33:06 +0530

    pointcut example

--- src/com/atuldwivedi/carcentre/aspect/LoggingAspect.java
+++ src/com/atuldwivedi/carcentre/aspect/LoggingAspect.java
@@ -1,15 +1,25 @@
 package com.atuldwivedi.carcentre.aspect;
 
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+	
+	@Pointcut("execution(* com.atuldwivedi.carcentre.*.*.*(..))")
+	public void allMethods() {}
 
-	@Before("execution(* com.atuldwivedi.carcentre.*.*.*(..))")
+	@Before("allMethods()")
 	public void methodEntryLog() {
-		System.out.println("=============> Method started");
+		System.out.println("==> Method started");
+	}
+	
+	@AfterReturning("execution(* com.atuldwivedi.carcentre.*.*.*(..))")
+	public void emthodExit() {
+		System.out.println("<== Method finished");
 	}
 }
